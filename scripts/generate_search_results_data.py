1dfae565db500a96aecd8cc9936f0ea80afb16d3
Martin Smyllie <martin@smyllie.com>
2024-06-19 09:16:10 +0100

    Improvements to generation of search results

--- scripts/generate_search_results_data.py
+++ scripts/generate_search_results_data.py
@@ -1,69 +1,74 @@
 import json
-import psycopg2
+import os, glob
+from postgres import Postgres
+from urllib.parse import quote_plus
 import pandas as pd
 
+POSTGRES_SERVER_NAME = os.environ.get("POSTGRES_SERVER")
+POSTGRES_SERVER_ADMIN_NAME = os.environ.get("POSTGRES_SERVER_ADMIN_LOGIN")
+POSTGRES_SERVER_ADMIN_PASSWORD = os.environ.get("POSTGRES_SERVER_ADMIN_PASSWORD")
 
-with open("app/backend/data/search_queries/top_searches.json", 'r') as file:
-    searches = json.load(file)
+POSTGRES_CMS_SERVER_NAME = os.environ.get("POSTGRES_CMS_SERVER")
+POSTGRES_CMS_ADMIN_NAME = os.environ.get("POSTGRES_CMS_ADMIN_LOGIN")
+POSTGRES_CMS_ADMIN_PASSWORD = os.environ.get("POSTGRES_CMS_ADMIN_PASSWORD")
 
-resultsDConnection = psycopg2.connect(host='postgres-jiuv4g2nm7sh2.postgres.database.azure.com', database='postgres', user='resultsadmin', password='PeteIsSk1ll-')
-wagtailDb = psycopg2.connect(host='nhsuk-cms-psql-dev-uks.postgres.database.azure.com', database='nhsuk-cms-review-comcomsearchpoc', user='wagtail@nhsuk-cms-psql-dev-uks', password=',Q-b-/<^)n95D99P')
+results_db_connection = Postgres(f"postgresql://{POSTGRES_SERVER_ADMIN_NAME}:{quote_plus(POSTGRES_SERVER_ADMIN_PASSWORD)}@{POSTGRES_SERVER_NAME}.postgres.database.azure.com:5432/postgres?sslmode=require")
+wagtail_db_connection = Postgres(f"postgresql://{quote_plus(POSTGRES_CMS_ADMIN_NAME)}:{quote_plus(POSTGRES_CMS_ADMIN_PASSWORD)}@{POSTGRES_CMS_SERVER_NAME}.postgres.database.azure.com:5432/nhsuk-cms-review-comcomsearchpoc?sslmode=require")
 
-resultsCursor = resultsDConnection.cursor()
-wagtailCursor = wagtailDb.cursor()
+path = './app/backend/data/search_queries'
+for filename in glob.glob(os.path.join(path, '*.json')):
+   
+    print(filename)
 
-for query in searches:
-    i = query["query"]
-    result = resultsCursor.execute(f"select * from public.poc_combined_rrf('{i}')")
+    with open(os.path.join(os.getcwd(), filename), 'r') as f: # open in readonly mode
 
-    list = []
-    output = []
-    rank = 1
-    csvFileName = f"./results/search_results_{i}.csv"
-    jsonFileName = f"./results/search_results_{i}.json"
+        for search_query in json.load(f):
+          
+            query = search_query["query"]
 
-    for j in resultsCursor:
-        # print("/" + j[0].replace("_", "/") + "/")
-        url = "/" + j[0].replace("_", "/") + "/"
-        list.append(url)
-        output.append({"url": url, "score": str(j[1]), "rank": rank})
-        rank += 1
+            list = []
+            output = []
+            rank = 1
+            csvFileName = f"./results/search_results_{query}.csv"
+            jsonFileName = f"./results/search_results_{query}.json"
 
-    sql = f"""
-        SELECT
-        slug,
-        title,
-        url_path
-        FROM public.wagtailcore_page
-        WHERE url_path IN ('{"','".join(list)}')
-    """
+            for j in results_db_connection.all(f"select * from public.poc_combined_rrf('{query}')"):
+                url = "/" + j[0].replace("_", "/") + "/"
+                list.append(url)
+                output.append({"url": url, "score": str(j[1]), "rank": rank})
+                rank += 1
 
-    wagtailCursor.execute(sql)
-    for page in wagtailCursor:
-        # print(page)
-        url = page[2]
-        outputresult = next((item for item in output if item.get("url") == url), None)
-        outputresult["title"] = page[1]
-        outputresult["slug"] = page[0]
+                sql = f"""
+                    SELECT
+                    slug,
+                    title,
+                    url_path
+                    FROM public.wagtailcore_page
+                    WHERE url_path IN ('{"','".join(list)}')
+                """
 
-    
-        
-    print(output)
-    print("         ")
+                for page in wagtail_db_connection.all(sql):
+                    # print(page)
+                    url = page[2]
+                    output_result = next((item for item in output if item.get("url") == url), None)
+                    output_result["title"] = page[1]
+                    output_result["slug"] = page[0]
 
-    # Save the JSON data to a file
-    with open(jsonFileName, 'w') as file:
-        json.dump(output, file)
+            print(output)
 
-    # Load the JSON data
-    json_data = json.dumps(output)
+            # Save the JSON data to a file
+            with open(jsonFileName, 'w') as file:
+                json.dump(output, file)
 
-    # Parse the JSON data into a Python dictionary
-    data = pd.read_json(json_data)
+            # Load the JSON data
+            json_data = json.dumps(output)
 
-    # Convert the dictionary to a DataFrame
-    df = pd.DataFrame(data)
+            # Parse the JSON data into a Python dictionary
+            data = pd.read_json(json_data)
 
-    # Write the DataFrame to a CSV file
-    df.to_csv(csvFileName, index=False)
+            # Convert the dictionary to a DataFrame
+            df = pd.DataFrame(data)
+
+            # Write the DataFrame to a CSV file
+            df.to_csv(csvFileName, index=False)
 
